165abcc2ed370f022ec21736511c1373e22f38ca
Airdropcel <muhamadrivaioke@gmail.com>
2026-08-12 09:43:14 +0700

    Create id.js

---
+++ id.js
@@ -0,0 +1,31 @@
+
+
+const langBtn=document.getElementById("langBtn");
+
+const langMenu=document.querySelector(".lang-menu");
+
+langBtn.onclick=function(){
+
+    langMenu.classList.toggle("show");
+
+}
+
+const lang = {
+
+id:{
+    navHome:"Beranda",
+    navMenu:"Menu",
+    navAbout:"Tentang",
+    navGal:"Galeri",
+    navMaps:"Lokasi",
+    navOrder:"Pesan",
+    navFaq:"FAQ",
+    navKon:"Kontak",
+
+
+
+},
+
+}
+
+};
