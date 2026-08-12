596dc958d8017bcb41a52ec0ec70c48811dfce82
Airdropcel <muhamadrivaioke@gmail.com>
2026-08-12 10:12:14 +0700

    Update language.js

--- language.js
+++ language.js
@@ -1,3 +1,25 @@
+const langBtn=document.getElementById("langBtn");
+
+const langMenu=document.querySelector(".lang-menu");
+
+langBtn.onclick=function(){
+
+    langMenu.classList.toggle("show");
+
+}
+
+const lang = {
+
+id:{
+    navHome:"Beranda",
+    navMenu:"Menu",
+    navAbout:"Tentang",
+    navGal:"Galeri",
+    navMaps:"Lokasi",
+    navOrder:"Pesan",
+    navFaq:"FAQ",
+    navKon:"Kontak",
+
 
 function setLang(language){
 
