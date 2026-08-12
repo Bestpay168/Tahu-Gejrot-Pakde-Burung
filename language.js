520f0254c8cb2d8803188beb2ae2408b5d8174ca
Airdropcel <muhamadrivaioke@gmail.com>
2026-08-12 10:14:32 +0700

    Update language.js

--- language.js
+++ language.js
@@ -30,6 +30,16 @@ en:{
     navFaq:"FAQ",
     navKon:"Kontak",
 
+zh:{
+    navHome:"首页",
+    navMenu:"菜单",
+    navAbout:"关于我们",
+    navGallery:"图库",
+    navMaps:"位置",
+    navOrder:"订购",
+    navFaq:"FAQ",
+    navKon:"Kontak",
+
 
 function setLang(language){
 
