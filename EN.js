11aeecd2ad5aa08fb190e910dc514db3b9ffb913
Airdropcel <muhamadrivaioke@gmail.com>
2026-08-12 09:45:04 +0700

    Create EN.js

---
+++ EN.js
@@ -0,0 +1,31 @@
+
+
+const langBtn=document.getElementById("langBtn");
+
+const langMenu=document.querySelector(".lang-menu");
+
+langBtn.onclick=function(){
+
+    langMenu.classList.toggle("show");
+
+}
+
+const lang = {
+
+
+en:{
+    navHome:"Home",
+    navMenu:"Menu",
+    navAbout:"About",
+    navGal:"Gallery",
+    navMaps:"Location",
+    navOrder:"Order",
+    navFaq:"FAQ",
+    navKon:"Kontak",
+
+},
+
+
+}
+
+};
