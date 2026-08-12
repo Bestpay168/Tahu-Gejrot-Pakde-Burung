8e19aea9bb9e708ecaf60d003087bac7f9d25f09
Airdropcel <muhamadrivaioke@gmail.com>
2026-08-12 09:49:06 +0700

    Create ja.js

---
+++ ja.js
@@ -0,0 +1,31 @@
+
+
+const langBtn=document.getElementById("langBtn");
+
+const langMenu=document.querySelector(".lang-menu");
+
+langBtn.onclick=function(){
+
+    langMenu.classList.toggle("show");
+
+}
+
+const lang = {
+
+
+ja:{
+    navHome:"ホーム",
+    navMenu:"メニュー",
+    navAbout:"会社概要",
+    navGallery:"ギャラリー",   
+    navMaps:"所在地",
+    navOrder:"注文",
+    navFaq:"FAQ",
+    navKon:"Kontak",
+
+},
+
+
+}
+
+};
