834f15e30665d8820e07e29cc19c76ccce89e87a
Airdropcel <muhamadrivaioke@gmail.com>
2026-08-17 21:08:49 +0700

    Update language.js

--- language.js
+++ language.js
@@ -535,7 +535,7 @@ function setLang(language){
 document.getElementById("navHome").textContent=lang[language].navHome;
 document.getElementById("navMenu").textContent=lang[language].navMenu;
 document.getElementById("navAbout").textContent=lang[language].navAbout;
-document.getElementById("navGal").textContent=lang[language].navGallery;
+document.getElementById("navGal").textContent=lang[language].navGal;
 document.getElementById("navMaps").textContent=lang[language].navMaps;
 document.getElementById("navOrder").textContent=lang[language].navOrder;
 document.getElementById("navFaq").textContent=lang[language].navFaq;
